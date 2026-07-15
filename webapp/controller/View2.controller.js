c1a820b26ead37fe90802aa656a460170410f8ac
Ayan Nath <mymail-ayaan@yahoo.co.in>
2026-07-16 01:53:06 +0530

    navigation route

--- webapp/controller/View2.controller.js
+++ webapp/controller/View2.controller.js
@@ -6,8 +6,11 @@ sap.ui.define([
     return Controller.extend("com.clientname.b70sapui5.controller.View2", {
         onInit() {
         },
-        onButtonPress() {
+        onButtonPress2() {
             // Handle button press event
+            // this.getOwnerComponent().getRouter().navTo("RouteMain");
+            // Go back to the previous page in the browser history
+            history.go(-1);
         }
     });
 });
